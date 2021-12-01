4c786a2e836c5b6dbfecb4e60f84b9955149ae6c
jumyeong <jm01028250379@gmail.com>
2021-12-01 22:52:19 +0900

    add: UserListView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views  import View
+from django.http import JsonResponse
+
+from users.models import User
+
+class ListView(View) :
+    def get(self, request) :
+        users = User.objects.all()
+        result = []
+
+        if users is None :
+            return JsonResponse({'MESSAGE': 'No User'}, status=200)
+
+        for user in users :
+            result.append({
+                "name"         : user.name,
+                "nickname"     : user.nickname,
+                "email"        : user.email,
+                "phone_number" : user.phone_number,
+                "github"       : user.github,
+                "profile_photo": user.profile_photo,
+                "description"  : user.description,
+                "position"     : user.position,
+                "created_at"   : user.created_at
+            })
+
+        return JsonResponse({'SUCCESS': result}, status=200)
