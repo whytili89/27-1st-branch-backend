1cb71658c0567af3310072f9f41baa0db3a06685
jumyeong <jm01028250379@gmail.com>
2021-12-02 15:28:56 +0900

    modify models.py
    related_name of manytoomany feild changed
     - users -> utags
     - postings -> ptags

--- branch_tags/models.py
+++ branch_tags/models.py
@@ -6,7 +6,7 @@ from keywords.models           import Keyword
 
 class UserTag(models.Model):
     name         = models.CharField(max_length=45, unique=True)
-    user         = models.ManyToManyField(User, related_name='users')
+    user         = models.ManyToManyField(User, related_name='utags')
     created_at   = models.DateField(auto_now_add=True)
     updated_at   = models.DateField(auto_now=True)
 
@@ -15,7 +15,7 @@ class UserTag(models.Model):
 
 class PostingTag(models.Model):
     name         = models.CharField(max_length=45, unique=True)
-    posting      = models.ManyToManyField(Posting, related_name='posts')
+    posting      = models.ManyToManyField(Posting, related_name='ptags')
     keyword      = models.ForeignKey(Keyword, on_delete=models.CASCADE)
     created_at   = models.DateField(auto_now_add=True)
     updated_at   = models.DateField(auto_now=True)
