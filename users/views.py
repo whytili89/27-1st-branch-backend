8018b9e2067cf8c5968c0a2d328a77404eec04d1
장도원 <jangdowon@dopago.local>
2021-12-02 14:01:32 +0900

    add: 로그인 기능구현

--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
-import json, re
+import json, bcrypt, jwt
 
 from django.views import View
 from django.http  import JsonResponse, HttpResponse
 
 from .models      import User
-from my_settings  import SECRET_KEY
+from my_settings  import SECRET_KEY, ALGORITHM
 from .validation  import validate_email, validate_phone_number, validate_password
 
 class SignUpView(View):
@@ -41,6 +41,35 @@ class SignUpView(View):
                 position      = position
             )
             return JsonResponse({'message':'SUCCESS!'}, status=201)
+
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+
+class SignInView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            email = data.get('email')
+            phone_number = data.get('phone_number')
+
+            if User.objects.filter(email=email).exists():
+                if bcrypt.checkpw(data['password'].encode('utf-8'), User.objects.get(email=email).password.encode('utf-8')):
+                    token = jwt.encode({'id':User.objects.get(email=email).id}, SECRET_KEY, algorithm=ALGORITHM)
+                    return JsonResponse({'MESSAGE':'SUCCESS', 'TOKEN':token}, status=200)
+                    
+            if User.objects.filter(phone_number=phone_number).exists():
+                if bcrypt.checkpw(data['password'].encode('utf-8'), User.objects.get(phone_number=phone_number).password.encode('utf-8')):
+                    token = jwt.encode({'id':User.objects.get(phone_number=phone_number).id}, SECRET_KEY, algorithm=ALGORITHM)
+                    return JsonResponse({'MESSAGE':'SUCCESS', 'TOKEN':token}, status=200)
+            
+            return JsonResponse({'message':'INVALID_USER'}, status=401)
+
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+        except User.DoesNotExist:
+            return JsonResponse({'message':'INVALID_USER'}, status=401)
+
 
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
