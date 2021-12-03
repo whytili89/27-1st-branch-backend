810721f40535d3d042e9a6c394b88f7368ac6d57
이태연 <itaeyeon@itaeyeon-ui-MacBookAir.local>
2021-12-03 18:43:39 +0900

    [Modify] commits record rebased

--- users/urls.py
+++ users/urls.py
@@ -1,17 +1,9 @@
 from django.urls import path
-<<<<<<< HEAD
-from .views      import SignUpView, SignInView
+
+from .views      import SignUpView, SignInView, UserProfileView
 
 urlpatterns = [
     path('/signup', SignUpView.as_view()),
-    path('/signin', SignInView.as_view())
+    path('/signin', SignInView.as_view()),
+    path('/<int:user_id>', UserProfileView.as_view())
 ]
-=======
-from .views      import SignUpView
-from .views      import MyPageView
-
-urlpatterns = [
-    path('/signup', SignUpView.as_view()),
-    path('/<int:user_id>', MyPageView.as_view())
-]
->>>>>>> 376aa32 (modify: .gitignore에 csv추가)
