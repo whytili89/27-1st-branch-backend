d9856aa536a0c19ef1c7f7fe54e142de3cbd038f
jumyeong <jm01028250379@gmail.com>
2021-12-03 16:27:30 +0900

    add : UserTagList

--- branch_tags/views.py
+++ branch_tags/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from django.http  import JsonResponse
+
+from .models import UserTag
+
+class UserTagListView(View) :
+    def get(self, request) :
+        userTags = UserTag.objects.all()
+
+        if userTags is None : 
+            return JsonResponse({'MESSAGE' : 'NO_USER_TAG'})
+
+        userTag_list = [{'tag_name' : userTag.name} for userTag in userTags]
+
+        return JsonResponse({'result' : userTag_list}, status=200)
+
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
+
 from .views      import SignUpView, SignInView
 
 urlpatterns = [
     path('/signup', SignUpView.as_view()),
-    path('/signin', SignInView.as_view())
+    path('/signin', SignInView.as_view()),
 ]
