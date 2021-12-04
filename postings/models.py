0422a73cf242d0db6f15a1002fb0170e64a8da83
이태연 <itaeyeon@itaeyeon-ui-MacBookAir.local>
2021-12-04 22:10:01 +0900

    migrate

--- postings/models.py
+++ postings/models.py
@@ -1,4 +1,5 @@
 from django.db                 import models
+from django.db.models.deletion import CASCADE
 
 from users.models              import User
 from keywords.models           import Keyword
@@ -8,7 +9,7 @@ class Posting(TimeStampModel):
     title         = models.CharField(max_length=100)
     sub_title     = models.CharField(max_length=200)
     content       = models.TextField()
-    thumbnail     = models.URLField()
+    thumbnail     = models.URLField(max_length=1000)
     user          = models.ForeignKey(User, on_delete=models.CASCADE)
     keyword       = models.ForeignKey(Keyword, on_delete=models.CASCADE)
 
@@ -24,6 +25,7 @@ class Like(models.Model):
 
 class Comment(TimeStampModel):
     user         = models.ForeignKey(User, on_delete=models.CASCADE)
+    posting      = models.ForeignKey('Posting', on_delete=models.CASCADE, null=True)
     reply        = models.TextField()
 
     class Meta:
