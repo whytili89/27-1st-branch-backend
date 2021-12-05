c940015442346b7f53b92ab6d6498b9c5754eef2
jumyeong <jm01028250379@gmail.com>
2021-12-06 00:43:36 +0900

    add :  Keyword list

--- keywords/views.py
+++ keywords/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views import View
+from django.http  import JsonResponse
 
-# Create your views here.
+from .models import Keyword
+
+class KeywordListView(View) :
+        def get(self, request) :
+            keywords = Keyword.objects.all()
+
+            keyword_list = [{
+                'keyword_id' : keyword.id,
+                'name' : keyword.name} for keyword in keywords]
+
+            return JsonResponse({'result' : keyword_list}, status=200)
--- teambranch/urls.py
+++ teambranch/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('users', include('users.urls')),
     path('branch_tags', include('branch_tags.urls')),
+    path('keywords', include('keywords.urls')),
 ]
