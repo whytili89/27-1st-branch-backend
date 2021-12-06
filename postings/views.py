de5a3722d27f121decf87f492672ec74f3f2eb93
장도원 <jangdowon@dopago.local>
2021-12-06 22:30:18 +0900

    modify: limit 100으로 수정, 쿼리 최적화, 태그 리스트by keyword 추가

--- postings/views.py
+++ postings/views.py
@@ -6,10 +6,10 @@ from .models          import Posting
 class PostListView(View):
     def get(self, request, keyword_id):
         order_method = request.GET.get('sort_method', 'created_at')
-        limit        = int(request.GET.get('limit', 5))
+        limit        = int(request.GET.get('limit', 100))
         offset       = int(request.GET.get('offset', 0))
 
-        posts = Posting.objects.filter(keyword_id=keyword_id).order_by(order_method)[offset:limit]
+        posts = Posting.objects.filter(keyword_id=keyword_id).select_related('user').order_by(order_method)[offset:limit]
 
         results = [{
             'title'      : post.title,
@@ -17,7 +17,8 @@ class PostListView(View):
             'content'    : post.content,
             'thumbnail'  : post.thumbnail,
             'user'       : post.user.nickname,
-            'created_at' : post.created_at } for post in posts
+            'created_at' : post.created_at, 
+            'tag'        : list(post.keyword.postingtag_set.values('name')) } for post in posts
             ]
         
         return JsonResponse({'result':results}, status=200)
