b6e1d6161b803bc6c4bcbca2ff6d7f02d4048de3
이태연 <itaeyeon@itaeyeon-ui-MacBookAir.local>
2021-12-06 21:27:10 +0900

    add: posting_tags

--- postings/views.py
+++ postings/views.py
@@ -3,23 +3,27 @@ import json
 from django.views import View
 from django.http  import JsonResponse
 
-from .models import Posting
+from .models            import Posting
+from branch_tags.models import PostingTag
+
 
 class PostView(View):
     def get(self,request,post_id):
         try:
             posting = Posting.objects.get(id=post_id)
+            posting_tags = Posting.objects.get(id=post_id).posting_tags.values("name")
+            
+            
             results = {
                 "title"       : posting.title,
                 "created_at"  : posting.created_at,
                 "content"     : posting.content,
                 "description" : posting.user.description,
-                "nickname"    : posting.user.nickname
-            }
+                "nickname"    : posting.user.nickname,
+                "post_tags"   : posting_tags
+            } 
+
             return JsonResponse({"message": "SUCCESS", "results" : results })
 
         except Posting.DoesNotExist:
-            return JsonResponse({"message" : "INVALID_POSTING"}, status=401)
-
-        except Posting.MultipleObjectsReturned:
             return JsonResponse({"message" : "INVALID_POSTING"}, status=401)
