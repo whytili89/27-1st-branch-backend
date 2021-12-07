f4e835938ed2285e07ed8b90dffa86bea8055a29
장도원 <jangdowon@dopago.local>
2021-12-07 17:40:50 +0900

    modify: manytomany through model추가, migrations 초기화

--- branch_tags/models.py
+++ branch_tags/models.py
@@ -1,4 +1,5 @@
 from django.db                 import models
+from django.db.models.deletion import CASCADE
 
 from users.models              import User
 from postings.models           import Posting
@@ -7,15 +8,30 @@ from core.models               import TimeStampModel
 
 class UserTag(TimeStampModel):
     name       = models.CharField(max_length=45, unique=True)
-    users      = models.ManyToManyField(User, related_name='user_tags')
+    users      = models.ManyToManyField(User, through='UsersUserTags', related_name='user_tags')
 
     class Meta:
         db_table = 'user_tags'
 
+class UsersUserTags(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    user      = models.ForeignKey(User, db_column='user_id', on_delete=CASCADE)
+    user_tag  = models.ForeignKey('UserTag', db_column='user_tag_id', on_delete=CASCADE)
+
 class PostingTag(TimeStampModel):
     name       = models.CharField(max_length=45, unique=True)
-    postings   = models.ManyToManyField(Posting, related_name='posting_tags')
+    postings   = models.ManyToManyField(Posting, through='PostingsPostingTags', related_name='posting_tags')
     keyword    = models.ForeignKey(Keyword, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'posting_tags'
+
+class PostingsPostingTags(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    posting      = models.ForeignKey(Posting, db_column='posting_id', on_delete=CASCADE)
+    posting_tag  = models.ForeignKey('PostingTag', db_column='posting_tag_id', on_delete=CASCADE)
+
+    class Meta:
+        db_table = 'postings_postingtags'
