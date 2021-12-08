1cabd25c957a54b58d163193d88fe932718506d5
jumyeong <jm01028250379@gmail.com>
2021-12-08 15:24:06 +0900

    modify : migrations

--- users/models.py
+++ users/models.py
@@ -1,8 +1,6 @@
 from core.models import TimeStampModel
 from django.db   import models
 
-from core.models import TimeStampModel
-
 class User(TimeStampModel):
     name          = models.CharField(max_length=45)
     nickname      = models.CharField(max_length=45)
@@ -13,7 +11,20 @@ class User(TimeStampModel):
     profile_photo = models.URLField(max_length=200, null=True)
     description   = models.TextField(null=True)
     position      = models.CharField(max_length=45, null=True)
-    subscribe     = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+    subscribe     = models.ManyToManyField('self', through='Subscribe', symmetrical=False)
 
     class Meta:
         db_table = 'users'
+
+class Subscribe(models.Model) :
+    subscribing = models.ForeignKey('User', on_delete=models.CASCADE, related_name='subscriber')
+    subscriber = models.ForeignKey('User', on_delete=models.CASCADE, related_name='subscribing')
+    
+    class Meta:
+        db_table = 'subscribe'
+
+        constraints = [
+            models.constraints.UniqueConstraint(
+            fields=['subscribing', 'subscriber'], name='unique_subscribing_subscriber'
+            )
+        ]
