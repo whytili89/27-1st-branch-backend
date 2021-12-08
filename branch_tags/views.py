0ed0c6c07d035109668a1e94099af68f8a558595
jumyeong <jm01028250379@gmail.com>
2021-12-08 11:19:43 +0900

    modify : user_tag list order_by method

--- branch_tags/views.py
+++ branch_tags/views.py
@@ -2,12 +2,16 @@ import json
 
 from django.views import View
 from django.http  import JsonResponse
-
+from django.db.models import Count
 from .models import UserTag
 
 class UserTagListView(View) :
     def get(self, request) :
-        user_tag = UserTag.objects.all()
+        limit  = int(request.GET.get('limit', 15))
+        offset = int(request.GET.get('offset', 0))
+
+        user_tag     = UserTag.objects.annotate(total=Count('usersusertags__user_tag_id')).order_by('-total')[offset:limit]
+
         userTag_list = [{'id' : userTag.id, 'tag_name' : userTag.name} for userTag in user_tag]
 
         return JsonResponse({'result' : userTag_list}, status=200)
