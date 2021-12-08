ae74e4b3bf2267f588dcd0d9b01459132637c89c
장도원 <jangdowon@dopago.local>
2021-12-08 21:53:12 +0900

    add: 포스트 좋아요 기능구현

--- postings/urls.py
+++ postings/urls.py
@@ -1,9 +1,11 @@
+from postings.models import Like
 from django.urls import path
 
-from .views      import PostListView
+from .views      import LikeView, PostListView
 from .views      import PostView
 
 urlpatterns=[
     path('/<int:keyword_id>', PostListView.as_view()),
     path('/datail/<int:posting_id>', PostView.as_view()),
+    path('/like', LikeView.as_view()),
 ]
--- postings/views.py
+++ postings/views.py
@@ -1,11 +1,12 @@
 import json
 
-from django.views import View
-from django.http  import JsonResponse
+from django.views     import View
+from django.http      import JsonResponse
+from json.decoder     import JSONDecodeError
 
-from .models      import Posting
-from .models      import Comment
-from core.utils   import login_decorator
+from .models          import Posting, Like
+from .models          import Comment
+from core.utils       import login_decorator
 
 class PostListView(View):
     def get(self, request, keyword_id):
@@ -28,7 +29,7 @@ class PostListView(View):
         return JsonResponse({'result':results}, status=200)
 
 class PostView(View):
-    def get(self,request,posting_id):
+    def get(self, request, posting_id):
         try:
             posting      = Posting.objects.get(id=posting_id)
             prev_posting = Posting.objects.filter(id__lt=posting_id, user_id=posting.user_id).values('id', 'title').order_by('-id')[:1]
@@ -52,3 +53,43 @@ class PostView(View):
 
         except Posting.DoesNotExist:
             return JsonResponse({"message" : "INVALID_POSTING"}, status=401)
+
+class LikeView(View):
+    @login_decorator
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            user = request.user
+
+            like, created = Like.objects.get_or_create(user_id = user.id, posting_id = data['posting_id'])
+
+            like.save()
+
+            print(like, created)
+
+            if not created:
+                like.delete()
+
+            return JsonResponse({'message':'SUCCESS'}, status=200)
+
+        except JSONDecodeError:
+          return JsonResponse({'message':'JSON_DECODE_EEROR'}, status=400)
+
+    @login_decorator
+    def get(self, request):
+        user    = request.user
+        postings = Posting.objects.filter(like__user_id = user.id)
+
+        try:
+            postings_user_liked = [{
+                'posting_id' : posting.id,
+                'like_count' : Like.objects.filter(posting_id = posting.id).count()
+            } for posting in postings]
+
+            return JsonResponse({
+                'message':'SUCCESS', 'POSTING_USER_LIKED' : postings_user_liked}, status=201
+            )
+        
+        except AttributeError:
+            return JsonResponse({'message':'AttributeError'}, status=400)
+        
