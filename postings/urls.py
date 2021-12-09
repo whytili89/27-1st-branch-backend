c4d4bd6c1e9de50f9fc3488646b540e9f6c27f5a
장도원 <jangdowon@dopago.local>
2021-12-09 14:44:28 +0900

    modify: conflict 해결

--- postings/urls.py
+++ postings/urls.py
@@ -6,6 +6,6 @@ from .views      import PostView
 
 urlpatterns=[
     path('/<int:keyword_id>', PostListView.as_view()),
-    path('/datail/<int:posting_id>', PostView.as_view()),
+    path('/detail/<int:posting_id>', PostView.as_view()),
     path('/like', LikeView.as_view()),
 ]
