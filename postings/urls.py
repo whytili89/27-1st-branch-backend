2e45a2b16c36c8621196c066af794c04581d0491
이태연 <itaeyeon@itaeyeon-ui-MacBookAir.local>
2021-12-09 11:38:16 +0900

    temporary commit

--- postings/urls.py
+++ postings/urls.py
@@ -6,7 +6,7 @@ from .views       import CommentView
 
 urlpatterns=[
     path('/<int:keyword_id>', PostListView.as_view()),
-    path('/datail/<int:posting_id>', PostView.as_view()),
+    path('/detail/<int:posting_id>', PostView.as_view()),
     path('/<int:posting_id>/comment', CommentView.as_view()),
     path('/<int:comment_id>/delete', CommentView.as_view()),
 ]
